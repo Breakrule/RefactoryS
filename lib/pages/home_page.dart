f3f7567a717dc75686eccc61792e0e14d1faf2c4
Rezky Maulana <rezkymaulanagt@gmail.com>
2023-05-31 13:00:28 +0800

    add alternatvie style

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:persistent_bottom_nav_bar_v2/persistent-tab-view.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,14 +10,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int _selectedIndex = 0;
-
-  List pages = [
-    const Center(child: Text("1")),
-    const Center(child: Text("2")),
-    const Center(child: Text("3")),
-    const Center(child: Text("4")),
-    const Center(child: Text("5")),
-  ];
+  late PersistentTabController _controller;
 
   void onTapNav(int index) {
     setState(() {
@@ -25,40 +19,90 @@ class _HomePageState extends State<HomePage> {
   }
 
   @override
+  void initState() {
+    super.initState();
+    _controller = PersistentTabController(initialIndex: 0);
+  }
+
+  List<Widget> _buildScreens() {
+    return [
+      Container(child: Center(child: Text("1"))),
+      Container(child: Center(child: Text("2"))),
+      Container(child: Center(child: Text("3"))),
+      Container(child: Center(child: Text("4"))),
+      Container(child: Center(child: Text("5"))),
+    ];
+  }
+
+  List<PersistentBottomNavBarItem> _navBarsItems() {
+    return [
+      PersistentBottomNavBarItem(
+        icon: const Icon(Icons.rocket_launch),
+        title: "Lobby",
+        activeColorPrimary: Colors.orange,
+        inactiveColorPrimary: Colors.grey,
+      ),
+      PersistentBottomNavBarItem(
+        icon: const Icon(Icons.chat),
+        title: "Chat",
+        activeColorPrimary: Colors.orange,
+        inactiveColorPrimary: Colors.grey,
+      ),
+      PersistentBottomNavBarItem(
+        icon: const Icon(Icons.list_alt),
+        title: "List",
+        activeColorPrimary: Colors.orange,
+        inactiveColorPrimary: Colors.grey,
+      ),
+      PersistentBottomNavBarItem(
+        icon: const Icon(Icons.star),
+        title: "Achievements",
+        activeColorPrimary: Colors.orange,
+        inactiveColorPrimary: Colors.grey,
+      ),
+      PersistentBottomNavBarItem(
+        icon: const Icon(Icons.person_pin),
+        title: "Me",
+        activeColorPrimary: Colors.orange,
+        inactiveColorPrimary: Colors.grey,
+      ),
+    ];
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: pages[_selectedIndex],
-      bottomNavigationBar: BottomNavigationBar(
-          selectedItemColor: Colors.blue,
-          unselectedItemColor: Colors.amberAccent,
-          showSelectedLabels: false,
-          showUnselectedLabels: false,
-          selectedFontSize: 0.0,
-          unselectedFontSize: 0.0,
-          currentIndex: _selectedIndex,
-          onTap: onTapNav,
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.rocket),
-              label: "Lobby",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.message),
-              label: "Message",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.task),
-              label: "Task",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.bookmark),
-              label: "Trophy",
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.person_2),
-              label: "About",
-            ),
-          ]),
+    return PersistentTabView(
+      context,
+      controller: _controller,
+      screens: _buildScreens(),
+      items: _navBarsItems(),
+      confineInSafeArea: true,
+      backgroundColor: Colors.white, // Default is Colors.white.
+      handleAndroidBackButtonPress: true, // Default is true.
+      resizeToAvoidBottomInset:
+          true, // This needs to be true if you want to move up the screen when keyboard appears. Default is true.
+      stateManagement: true, // Default is true.
+      hideNavigationBarWhenKeyboardShows:
+          true, // Recommended to set 'resizeToAvoidBottomInset' as true while using this argument. Default is true.
+      decoration: NavBarDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        colorBehindNavBar: Colors.white,
+      ),
+      popAllScreensOnTapOfSelectedTab: true,
+      popActionScreens: PopActionScreensType.all,
+      itemAnimationProperties: const ItemAnimationProperties(
+        // Navigation Bar's items animation properties.
+        duration: Duration(milliseconds: 200),
+        curve: Curves.ease,
+      ),
+      screenTransitionAnimation: const ScreenTransitionAnimation(
+        // Screen transition animation on change of selected tab.
+        animateTabTransition: true,
+        curve: Curves.ease,
+        duration: Duration(milliseconds: 200),
+      ),
+      navBarStyle:
+          NavBarStyle.style11, // Choose the nav bar style with this property.
     );
   }
 }
